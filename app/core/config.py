c00555ebd2567c20fc9d01b6a650cbb14ea6acda
Aman Verma <coderaman18@gmail.com>
2026-06-15 21:09:21 +0530

    Render issue fixed

--- app/core/config.py
+++ app/core/config.py
@@ -19,3 +19,16 @@ settings.DATABASE_URL = (
     .replace("postgresql://", "postgresql+psycopg://")
     .replace("postgresql+psycopg2://", "postgresql+psycopg://")
 )
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    LANGGRAPH_DB_URL: str
+
+    GROQ_API_KEY: str
+    GROQ_MODEL: str
+    GROQ_BASE_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
--- app/graph/graph.py
+++ app/graph/graph.py
@@ -2,7 +2,9 @@ from langgraph.checkpoint.postgres import PostgresSaver
 from app.core.config import settings
 from app.workflows.workflow_builder import builder
 
-_checkpointer_cm = PostgresSaver.from_conn_string(settings.DATABASE_URL)
+_checkpointer_cm = PostgresSaver.from_conn_string(
+    settings.LANGGRAPH_DB_URL
+)
 checkpointer = _checkpointer_cm.__enter__()
 
 def get_graph():
